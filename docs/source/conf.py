10d153f1afb86e3356ac8286fd56b5e9c51f7337
KenBigelow <44417562+KenBigelow@users.noreply.github.com>
2024-06-13 11:14:10 -0700

    Create conf.py

---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_logo = "bcnet-logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,     
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False,
+    'globaltoc_collapse': False,
+    'globaltoc_maxdepth': None,
+}
